cc64bdb90ddcf21722ce395da7d5d56d76c80312
Md Parvej <mdparvejme24@gmail.com>
2024-09-09 15:24:09 +0600

    solve issue from main.jsx file

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
-import router from "./routers/Router.js";
+import router from "./routers/Router";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
