144143c934973a43d64df2d33b27876e97832049
Md Parvej <mdparvejme24@gmail.com>
2024-09-13 14:11:56 +0600

    update course resource style

--- src/components/CollegeDetailsComponents/CourseResearch/CourseResearch.jsx
+++ src/components/CollegeDetailsComponents/CourseResearch/CourseResearch.jsx
@@ -1,20 +1,29 @@
+import { FaGraduationCap } from "react-icons/fa6";
+import { HiDocumentMagnifyingGlass } from "react-icons/hi2";
+import { IoGameControllerSharp } from "react-icons/io5";
+
 export default function CourseResearch({
   courses,
   researches,
   sportsFacilities,
 }) {
   return (
-    <div>
+    <div className="grid grid-cols-1 md:grid-cols-3 gap-3">
       {/* courses side  */}
       <div>
-        <h2 className="text-2xl font-bold">Courses</h2>
+        <h2 className="text-2xl font-bold flex items-center gap-2 pb-2">
+          <span className="text-blue-600 text-3xl">
+            <FaGraduationCap />
+          </span>{" "}
+          Courses
+        </h2>
         <div>
           {courses.map((course) => (
             <div
               key={course.index}
-              className="text-lg bg-blue-50 py-2 px-4 mb-1"
+              className="text-lg bg-blue-50 py-2 px-4 mb-1 flex items-center gap-2"
             >
-              {course}
+              <FaGraduationCap /> {course}
             </div>
           ))}
         </div>
@@ -22,14 +31,19 @@ export default function CourseResearch({
 
       {/* researches section  */}
       <div>
-        <h2 className="text-2xl font-bold">Researches</h2>
+        <h2 className="text-2xl font-bold flex items-center gap-2 pb-2">
+          <span className="text-blue-600 text-3xl">
+            <HiDocumentMagnifyingGlass />
+          </span>
+          Researches
+        </h2>
         <div>
           {researches.map((research) => (
             <div
               key={research.index}
-              className="text-lg bg-blue-50 py-2 px-4 mb-1"
+              className="text-lg bg-blue-50 py-2 px-4 mb-1 flex items-center gap-2"
             >
-              {research}
+              <HiDocumentMagnifyingGlass /> {research}
             </div>
           ))}
         </div>
@@ -37,14 +51,19 @@ export default function CourseResearch({
 
       {/* sportsFacilities section  */}
       <div>
-        <h2 className="text-2xl font-bold">Researches</h2>
+        <h2 className="text-2xl font-bold flex items-center gap-2 pb-2">
+          <span className="text-blue-600 text-3xl">
+            <IoGameControllerSharp />
+          </span>
+          Researches
+        </h2>
         <div>
           {sportsFacilities.map((sporse) => (
             <div
               key={sporse.index}
-              className="text-lg bg-blue-50 py-2 px-4 mb-1"
+              className="text-lg bg-blue-50 py-2 px-4 mb-1 flex items-center gap-2"
             >
-              {sporse}
+              <IoGameControllerSharp /> {sporse}
             </div>
           ))}
         </div>
